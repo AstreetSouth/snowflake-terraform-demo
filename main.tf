7cf0ca8b94e1f031386cadb3039d0c47295291ec
Robert Atkin <m3n3ue@splm.siemens.com>
2022-05-07 14:31:12 -0700

    test 2nd db add

--- main.tf
+++ main.tf
@@ -19,12 +19,12 @@ provider "snowflake" {
 }
 
 resource "snowflake_database" "dev_demo_db" {
-  name    = "DEV_DEMO_DB"
+  name    = "DEV_DEMO_DB_v2"
   comment = "Database for Snowflake Terraform demo"
 }
 
 resource "snowflake_schema" "dev_demo_schema" {
-  database = snowflake_database.dev_demo_db.name
+  database = snowflake_database.dev_demo_db_v2.name
   name     = "DEMO_SCHEMA"
   comment  = "Schema for Snowflake Terraform demo"
 }
